91be96753b26649630a9a3d487149cbdafa10c69
joaovitorevora <joaovitorevora@gmail.com>
2023-10-06 14:51:27 -0300

    inserindo php para testes

---
+++ assets/php/cadastro.php
@@ -0,0 +1,43 @@
+<?php
+// Verifique se o formulário foi submetido
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Conecte-se ao banco de dados MySQL
+    $conn = new mysqli("localhost:3306", "root", "root", "pizzaria");
+
+    // Verifique a conexão
+    if ($conn->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    }
+
+    // Captura os dados do formulário
+    $nome = $_POST["nome_cad"];
+    $cpf = $_POST["cpf_cad"];
+    $email = $_POST["email_cad"];
+    $senha = $_POST["senha_cad"];
+
+    // Prepara a consulta SQL para inserir o cliente
+    $sql = "INSERT INTO clientes (nome, cpf, email, senha) VALUES (?, ?, ?, ?)";
+
+    // Prepara a declaração SQL
+    $stmt = $conn->prepare($sql);
+
+    // Verifique se a preparação da consulta foi bem-sucedida
+    if ($stmt === false) {
+        die("Erro na preparação da consulta: " . $conn->error);
+    }
+
+    // Associa os parâmetros à declaração
+    $stmt->bind_param("ssss", $nome, $cpf, $email, $senha);
+
+    // Executa a consulta
+    if ($stmt->execute()) {
+        echo "Cliente cadastrado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar o cliente: " . $stmt->error;
+    }
+
+    // Fecha a declaração e a conexão
+    $stmt->close();
+    $conn->close();
+}
+?>
